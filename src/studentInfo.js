3f20b4f2e81c5739d74704df273b63b0d2689343
DeeKoders <deekoders@gmail.com>
2022-01-05 17:16:29 +0500

    Added a React Hook
    useState hook is added in studentInfo.js to increase and Decrease Student Age on Button Click

--- src/studentInfo.js
+++ src/studentInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { useState } from 'react';
 function StudentInfo(props){
+    let [age, setAge] = useState(props.stdAge)
     return(
         <div>
             <center>
@@ -8,8 +9,13 @@ function StudentInfo(props){
                 <hr/>
                 <h2>Student Name: {props.stdName}</h2>
                 <h2>Student Program: {props.stdProgram}</h2>
-                <h2>Student Age: {props.stdAge}</h2>
+
+
+                <h2>Student Age: {age}</h2><button onClick={()=> setAge(age+1)}>Increase</button><button onClick={()=> setAge(age-1)}>Decrease</button>
+
+
                 <h2>Institute Name: {props.stdInstitute}</h2>
+
             </center>
         </div>
     );
