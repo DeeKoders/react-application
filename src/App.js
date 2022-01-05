f6d0c2e06c5f26d11dab0dee7a927db0dd157c3e
DeeKoders <deekoders@gmail.com>
2022-01-05 17:19:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+
 import StudentInfo from './studentInfo';
 
 
 function App() {
   return (
-    <StudentInfo stdName = "Danyal Ahmed" stdAge = "21" stdProgram = "MERN Stack" stdInstitute = "IMSciences" />
+    <StudentInfo stdName = "Danyal Ahmed" stdAge = {21} stdProgram = "MERN Stack" stdInstitute = "IMSciences" />
   );
 }
 
